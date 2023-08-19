64e1df2f0e7cd2b1e22af3f74246510c7fb5a9ec
Lau Josefsen <lejo@cego.dk>
2023-08-19 16:49:44 +0200

    Made it use json format

--- src/main/kotlin/dk/cego/gitlab_ci_local_plugin/GclJob.kt
+++ src/main/kotlin/dk/cego/gitlab_ci_local_plugin/GclJob.kt
@@ -1,7 +1,10 @@
 package dk.cego.gitlab_ci_local_plugin
 
-class GclJob(
-    val name: String, private val description: String, val stage: String,
-    private val glWhen: String, private val allow_failure: Boolean,
-    private val needs: List<String>
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class GclJob(
+    val name: String,
+    val description: String,
+    val stage: String,
 )
